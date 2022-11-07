2dd338bc14c537f73baaee10ec283953db4dd4ca
miguelangellerma <miguellangel770@gmail.com>
2022-11-07 18:24:34 -0500

    document title fix

--- src/pages/delete-account/index.js
+++ src/pages/delete-account/index.js
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import ManTrashBin from "../../assets/svgr/illustration/ManTrashBin";
 import DeleteAccountForm from "./DeleteAccountForm";
 
 
 const DeleteAccountPage = () => {
+
+  useEffect(() => {
+    document.title = "Delete account";
+  }, []);
+
+
   return (
     <div className="w-full p-[20px] flex flex-col gap-[25px] h-full m-auto sm:mt-[52px] duration-200 overflow-auto max-h-full">
       <ManTrashBin className="mx-auto mt-auto sm:mt-0 w-[min(100%,450px)] min-h-[150px] animate-[hueShift_3s_linear_infinite_alternate] text-accent3"></ManTrashBin>
